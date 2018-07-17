28af3ad0bbf73ac5b8440d2e3227666d8e434560
nick <nli8888@hotmail.com>
2018-07-17 10:58:42 +0100

    Updated

---
+++ src/Paralogous_var_align_functions.R
@@ -0,0 +1,56 @@
+Packages = c("tidyverse", "dplyr", "ggplot2", "ggsignif", "huxtable")
+lapply(Packages, library, character.only = TRUE)
+
+Paralogous_var_align = function(paralogs2_file, paralog_tableized_file, joining_tableized_file=paralog_tableized_file){ #Function for joining together variant paralogous locations
+  paralog_data = file(paralogs2_file)
+  max_no_col = (max(count.fields(paralog_data, sep = "\t"))-6) #-6 for "Variant_pos", "ID", "Gene", "Ref", "Alt", and "\n"
+  paralog_data = read.csv(file=paralogs2_file, sep="\t", header=FALSE, col.names = c("Variant_pos", "ID", "Gene", "REF", "ALT", paste("paralog", 1:max_no_col, sep = "")))
+  paralog_tableized_data = read.csv(file=paralog_tableized_file, sep = "\t", header=TRUE, stringsAsFactors=FALSE)
+  paralog_tableized_data = paralog_tableized_data[!is.na(paralog_tableized_data$Amino_acids),]
+  
+  paralog_tableized_data$Variant_pos = paste(paralog_tableized_data$CHROM,paralog_tableized_data$POS, sep = " ")
+  paralog_tableized_data$REF_Amino_acids = sapply(paralog_tableized_data[,"Amino_acids"],strsplit, "/")
+  paralog_tableized_data$REF_Amino_acids = sapply(paralog_tableized_data[,"REF_Amino_acids"],unlist)
+  paralog_tableized_data$REF_Amino_acids = sapply(paralog_tableized_data[,"REF_Amino_acids"],function(x) x[1])
+  paralog_tableized_data$ALT_Amino_acids = sapply(paralog_tableized_data[,"Amino_acids"],strsplit, "/")
+  paralog_tableized_data$ALT_Amino_acids = sapply(paralog_tableized_data[,"ALT_Amino_acids"],unlist)
+  paralog_tableized_data$ALT_Amino_acids = sapply(paralog_tableized_data[,"ALT_Amino_acids"],function(x) x[2])
+  paralog_data = left_join(paralog_data,paralog_tableized_data, by =  c("Variant_pos", "ID", "REF", "ALT", "Gene" = "SYMBOL"))
+  gathered_paralog_data = filter(gather(paralog_data, paralog, paralog_pos, paste("paralog", 1:max_no_col, sep = ""), factor_key = TRUE), paralog_pos != "")
+  
+  joining_tableized_data = read.csv(file=joining_tableized_file, sep = "\t", header=TRUE, stringsAsFactors=FALSE)
+  joining_tableized_data = joining_tableized_data[!is.na(joining_tableized_data$Amino_acids),]
+  
+  joining_tableized_data$Variant_pos = paste(joining_tableized_data$CHROM,joining_tableized_data$POS, sep = " ")
+  joining_tableized_data$REF_Amino_acids = sapply(joining_tableized_data[,"Amino_acids"],strsplit, "/")
+  joining_tableized_data$REF_Amino_acids = sapply(joining_tableized_data[,"REF_Amino_acids"],unlist)
+  joining_tableized_data$REF_Amino_acids = sapply(joining_tableized_data[,"REF_Amino_acids"],function(x) x[1])
+  joining_tableized_data$ALT_Amino_acids = sapply(joining_tableized_data[,"Amino_acids"],strsplit, "/")
+  joining_tableized_data$ALT_Amino_acids = sapply(joining_tableized_data[,"ALT_Amino_acids"],unlist)
+  joining_tableized_data$ALT_Amino_acids = sapply(joining_tableized_data[,"ALT_Amino_acids"],function(x) x[2])
+  ref_data = joining_tableized_data
+  
+  Total_paralog_annotations = left_join(gathered_paralog_data, ref_data, by = c("paralog_pos" = "Variant_pos"))
+  num_of_paralog_anno = sum(!is.na(Total_paralog_annotations$ID.y))
+  return(list("paralog_data" = paralog_data, "gathered_paralog_data" = gathered_paralog_data, "Total_paralog_annotations" = Total_paralog_annotations, "num_of_paralog_anno" = num_of_paralog_anno, "ref_data" = ref_data))
+}
+
+conf_matrix = function(ptop.num_of_paralog_anno, p.paralog_data, btop.num_of_paralog_anno, b.paralog_data){
+  con_table_TP = ptop.num_of_paralog_anno
+  con_table_FP = btop.num_of_paralog_anno
+  con_table_TN = nrow(b.paralog_data)-btop.num_of_paralog_anno
+  con_table_FN = nrow(p.paralog_data)-ptop.num_of_paralog_anno
+  con_table_PPV = con_table_TP/(con_table_TP+con_table_FP)
+  con_table_Sensitivty = con_table_TP/(con_table_TP+con_table_FN)
+  con_table = matrix(
+    c(nrow(p.paralog_data),
+      ptop.num_of_paralog_anno,
+      nrow(b.paralog_data),
+      btop.num_of_paralog_anno
+    ), ncol = 2
+  )
+  colnames(con_table) = c("Pathogenic", "Benign")
+  rownames(con_table) = c("Number of variants in total", "Number of variants predicted as pathogenic")
+  con_table_p_value = fisher.test(con_table)
+  return(list("con_table" = con_table, "PPV" = con_table_PPV, "Sensitivity" = con_table_Sensitivty, "Pvalue" = con_table_p_value, "TP" = con_table_TP, "FP" = con_table_FP, "TN" = con_table_TN, "FN" = con_table_FN))
+}
