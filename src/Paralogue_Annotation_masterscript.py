4961fac2ce92b582494718b5400031d5997e936b
nick <nli8888@hotmail.com>
2018-10-02 16:20:57 +0100

    Updated

--- src/Paralogue_Annotation_masterscript.py
+++ src/Paralogue_Annotation_masterscript.py
@@ -3,15 +3,28 @@ from VEP_ParalogAnno import *
 from File_prep_for_R import *
 from Tableize_wrapper import *
 
-input_file = sys.argv[1]	#name of input file
+input_file = sys.argv[1]	#path of input file
+
+build = sys.argv[2]	#genome build: either 37 or 38
+if build == 37:
+	genome_build = "GRCh37"
+elif build == 38:
+	genome_build = "GRCh38"
+else:
+	sys.exit("ERROR: genome build not recognized")
+
+if flavour == "base":
+	flavour = 0
+elif flavour == "variant":
+	flavour = 1
+elif flavour == "paraloc":
+	flavour = 2
+else:
+	sys.exit("ERROR: mode selected not recognized")
 
 print(len(sys.argv), sys.argv)
 
-if len(sys.argv) == 4:
-	flavour = int(sys.argv[3])
-else:
-	flavour = 2
 
-VEP_Plugin_run(input_file, genome_build="GRCh38", flavour=flavour, VEPversion=93, offline=1)#, output_filename=sys.argv[2])
+VEP_Plugin_run(input_file, genome_build=genome_build, flavour=flavour, VEPversion=93, offline=1)#, output_filename=sys.argv[2])
 Tableize_wrap(input_file.rsplit(".",1)[0]+".out_paraloc")
 R_file_prep(input_file.rsplit(".",1)[0]+".out_paraloc_paralogs", "noQC")
