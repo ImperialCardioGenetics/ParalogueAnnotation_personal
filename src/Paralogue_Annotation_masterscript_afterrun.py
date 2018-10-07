98ca3dba113cc5ca1b3f8652e02679c439ece219
nli8888 <nli8888@hotmail.com>
2018-10-07 17:07:43 +0100

    Updated

---
+++ src/Paralogue_Annotation_masterscript_afterrun.py
@@ -0,0 +1,43 @@
+#Same as Paralogue_Annotation_masterscript.py but doesn't run VEP+plugin. So if .out_paraloc file already exists, save time by running this instead.
+
+import os, sys, subprocess, re, shlex, subprocess, codecs
+from VEP_ParalogAnno_wo_VEP import *
+from File_prep_for_R import *
+from Tableize_wrapper import *
+
+input_file = sys.argv[1]	#path of input file
+
+build = sys.argv[2]	#genome build: either 37 or 38
+if int(build) == 37:
+	genome_build = "GRCh37"
+elif int(build) == 38:
+	genome_build = "GRCh38"
+else:
+	sys.exit("ERROR: genome build not recognized")
+
+flavour = sys.argv[3] #flavour of plugin
+if flavour == "base":
+	flavour = 0
+elif flavour == "variant":
+	flavour = 1
+elif flavour == "paraloc": #NORMALLY paraloc IS WHAT YOU WANT
+	flavour = 2
+else:
+	sys.exit("ERROR: plugin mode selected not recognized")
+
+refid_flavour = sys.argv[4] #flavour of refid filtering - how conserved do you want the reference alleles to be?
+if refid_flavour == "noQC":
+	refid_flavour = "noQC"
+elif refid_flavour == "para_con":
+	refid_flavour = "para_con"
+elif refid_flavour == "all_con":
+	refid_flavour = "all_con"
+else:
+	sys.exit("ERROR: refid flavour selected not recognized")
+
+print(len(sys.argv), sys.argv)
+
+
+VEP_Plugin_afterrun(input_file, genome_build=genome_build, flavour=flavour, VEPversion=93, offline=1)#, output_filename=sys.argv[2])
+Tableize_wrap(input_file.rsplit(".",1)[0]+".out_paraloc")
+R_file_prep(input_file.rsplit(".",1)[0]+".out_paraloc_paralogs", refid_flavour=refid_flavour)
---
+++ src/VEP_ParalogAnno_wo_VEP.py
@@ -0,0 +1,60 @@
+import os, sys, subprocess, re, shlex, subprocess, codecs
+
+
+#Same as VEP_ParalogAnno.py but doesn't run VEP+Plugin as it assumes that has already been run. Saves time if .out_paraloc file has already by generated. See Paralogue_Annotation_masterscript_afterrun.py
+
+def VEP_Plugin_afterrun(input_file, flavour=2, genome_build="GRCh38", VEPversion=90, offline=1, output_filename="default"):
+	input_dir = input_file.rsplit("/", 1)[0]
+	if flavour == 0:
+		flavour = ""
+		output_file = input_file.rsplit(".",1)[0]+".out_no_plugin"
+		print(output_file)
+	elif flavour == 1:
+		flavour = "--plugin ParalogueAnnotation"
+		output_file = input_file.rsplit(".",1)[0]+".out"
+		print(output_file)
+	elif flavour == 2:
+		flavour = "--plugin ParalogueAnnotation,paraloc"
+		output_file = input_file.rsplit(".",1)[0]+".out_paraloc"
+		print(output_file)
+
+	if output_filename != "default":
+		output_file = output_filename	
+
+	if not input_dir.endswith("/"):
+		input_dir = input_dir+"/"
+	print(input_dir)
+
+	#reads through outfile of vep+plugin and tidies up data by extracting only variants that have paralogs and spits that out as another outfile
+	out_file = open(
+		output_file + "_paralogs", "w", encoding="utf-8")
+	if not flavour == "":
+		with codecs.open(	#changed "open" to "codecs.open" to solve unicode error. Changed back maybe
+			output_file, encoding="utf-8"
+			) as infile:
+			for line in infile:
+				if not line.startswith("#"):
+					line = line.split("CSQ=")
+					CSQ = line[0]
+					# print(CSQ)	#print query variant #commented out to avoid unicode error
+					line = line[1].split(",")
+					# print(line, "\n")
+					for x in line:
+						y = x.split("|")
+						gene = y[3]
+						paralog = y[25]	#Remember to change indexing if also changing the number of flags being used by VEP
+						if paralog == "" or paralog == "\n":
+							paralog_check = 0
+						else:
+							paralog_check = 1
+						if paralog_check == 1:
+							# print(x, "\n")	#commented out to avoid unicode error
+							out_file.write(
+								str(CSQ)+"PARALOGS->"
+								+str(x)+"\n"
+								)
+				else:
+					out_file.write(line)
+	out_file.close()
+	
+	print("VEP_ParalogAnno Done!")
