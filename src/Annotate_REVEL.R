6fca292bd76a320645ebed2ad4aae525ef3f1d74
nli8888 <nli8888@hotmail.com>
2018-11-19 20:47:16 +0000

    updated

---
+++ src/Annotate_REVEL.R
@@ -0,0 +1,9 @@
+
+# if (!file.exists("data/revel_all_chromosomes.csv.zip")){
+#   download.file("https://rothsj06.u.hpc.mssm.edu/revel/revel_all_chromosomes.csv.zip", "data/revel_all_chromosomes.csv.zip")
+# }
+
+REVEL_scores = read.csv(file=unz("/media/nick/821ED5711ED55F2B/Users/Nick/Documents/PhD/Paralogues/ParalogueAnnotation_personal/data/revel_all_chromosomes.csv.zip", "revel_all_chromosomes.csv"))
+
+
+
