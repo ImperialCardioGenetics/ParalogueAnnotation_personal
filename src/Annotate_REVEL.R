6411ac6477cfc63a8e54b8cd7d64c4dc8259af31
nick <nick@rbh-head.cm.cluster>
2018-11-29 18:32:14 +0000

    updated

--- src/Annotate_REVEL.R
+++
@@ -1,9 +0,0 @@
-
-# if (!file.exists("data/revel_all_chromosomes.csv.zip")){
-#   download.file("https://rothsj06.u.hpc.mssm.edu/revel/revel_all_chromosomes.csv.zip", "data/revel_all_chromosomes.csv.zip")
-# }
-
-# REVEL_scores = read.csv(file=unz("/media/nick/821ED5711ED55F2B/Users/Nick/Documents/PhD/Paralogues/ParalogueAnnotation_personal/data/revel_all_chromosomes.csv.zip", "revel_all_chromosomes.csv"))
-
-REVEL_scores = read.csv("/media/nick/Data/Users/N/Documents/PhD/Paralogues/data_files/test.csv")
-
