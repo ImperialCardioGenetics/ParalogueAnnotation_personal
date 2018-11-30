f5536fb735eb1af450134d353f5dad597fc99747
nli8888 <nli8888@hotmail.com>
2018-11-30 12:13:23 +0000

    updated

--- src/Compare_with_sift_revel.py
+++ src/Compare_with_sift_revel.py
@@ -4,3 +4,5 @@ import os, sys, subprocess, re, shlex, subprocess, codecs
 
 input_file1 = sys.argv[1] #Tableized file
 input_file2 = sys.argv[2] #paralogs file
+
+os.system("python /data/Share/nick/Paralog_Anno/loftee/src/tableize_vcf.py --vcf " + input_file2 + " --out " + input_file2 + "_tableized_org --do_not_minrep --include_id --vep_info SYMBOL,Protein_position,Amino_acids,Codons,Paralogue_Vars,SIFT_score,SIFT_score --split_by_transcript --canonical_only")
