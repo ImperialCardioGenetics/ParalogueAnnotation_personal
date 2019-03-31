e8f82ddd4c7460e5a5c1a0446f7ad98f2fc7817d
nli8888 <nli8888@hotmail.com>
2019-03-31 11:58:02 +0100

    updated

--- sql/ICC_Mutations.sql
+++ sql/ICC_Mutations.sql
@@ -3,6 +3,10 @@ SELECT gene_symbol, mut_id, vs_case_count, mut_exac_count, mut_coding, mut_prote
 FROM ICC_MUTATIONS.report, ICC_MUTATIONS.gene, ICC_MUTATIONS.mutation, ICC_MUTATIONS.variant_study
 WHERE rep_ref_id = 665 AND gene_id=mut_gene_id AND mut_id=rep_mut_id;
 
+SELECT mut_chromosome, mut_chr_start_source, mut_id, mut_coding
+FROM ICC_MUTATIONS.report, ICC_MUTATIONS.gene, ICC_MUTATIONS.mutation
+WHERE rep_ref_id = 665 AND gene_id=mut_gene_id AND mut_id=rep_mut_id;
+
 SELECT * FROM ICC_MUTATIONS.report, ICC_MUTATIONS.gene, ICC_MUTATIONS.mutation;
 
 
