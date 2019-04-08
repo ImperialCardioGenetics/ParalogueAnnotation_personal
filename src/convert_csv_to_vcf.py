8a979dd7a5c6ee29f9f955660fea5d94eeb7d878
nli8888 <nli8888@hotmail.com>
2019-04-08 20:15:40 +0100

    updated

--- src/convert_csv_to_vcf.py
+++ src/convert_csv_to_vcf.py
@@ -43,7 +43,7 @@ import  os, sys, subprocess, re
 # 				)			
 # out_file2.close()
 
-input_file = sys.argv[1]	#path of csv file to convert to vcf e.g. /media/nick/Data/PhD/Paralogues/ParalogueAnnotation_personal/data/LQTS/Mayo_LQTS_variants_for_vcf_convertion.csv
+input_file = sys.argv[1]	#path of ICC MUTATION csv file to convert to vcf e.g. /media/nick/Data/PhD/Paralogues/ParalogueAnnotation_personal/data/LQTS/Mayo_LQTS_variants_for_vcf_convertion.csv
 dir1 = input_file.rsplit("/", 1)[0]
 
 out_file = open(input_file.rsplit(".", 1)[0]+".vcf", "w")
@@ -69,3 +69,31 @@ with open(input_file) as f:
 				".\n"
 				)			
 out_file.close()
+
+input_file = sys.argv[2]	#path of gnomad csv file to convert to vcf e.g. /media/nick/Data/PhD/Paralogues/ParalogueAnnotation_personal/data/LQTS/gnomAD_v2.1.1_ENSG00000180509_2019_03_31_15_09_18_KCNE1.csv
+dir1 = input_file.rsplit("/", 1)[0]
+
+out_file = open(input_file.rsplit(".", 1)[0]+".vcf", "w")
+out_file.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n")
+
+with open(input_file) as f:
+	for line in f:
+		print(line)
+		if line[0].isdigit():
+			line = line.rstrip().split(",")
+			chrom = line[0]
+			pos = line[1]
+			ID = line[2]
+			ref = line[3].split(">")[0][-1]
+			alt = line[3].split(">")[1]
+			out_file.write(
+				str(chrom)+"\t"+
+				str(pos)+"\t"+
+				str(ID)+"\t"+
+				str(ref)+"\t"+
+				str(alt)+"\t.\t.\t"+
+				# "gene="+str(line[4])+",HGVSc="+str(line[5])+",HGVSp="+str(line[6])+",pathogenic="+str(line[7])+"\n"
+				".\n"
+				)			
+out_file.close()
+
