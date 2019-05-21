8364e82f61fb5ce29c9e435bad89d4090c6080a1
nli8888 <nli8888@hotmail.com>
2019-05-21 15:54:09 +0100

    updated

--- sql/ICC_Mutations.sql
+++ sql/ICC_Mutations.sql
@@ -3,20 +3,28 @@
 #FROM ICC_MUTATIONS.report, ICC_MUTATIONS.gene, ICC_MUTATIONS.mutation, ICC_MUTATIONS.variant_study
 #WHERE rep_ref_id = 665 AND gene_id=mut_gene_id AND mut_id=rep_mut_id;
 
+#For LQTS
 SELECT mut_chromosome, mut_chr_start_source, mut_id, mut_coding
 FROM ICC_MUTATIONS.report, ICC_MUTATIONS.gene, ICC_MUTATIONS.mutation
 WHERE rep_ref_id = 665 AND gene_id=mut_gene_id AND mut_id=rep_mut_id AND mut_effect = "missense";# AND mut_coding NOT LIKE "%del%";
 
-SELECT gene_symbol, mut_id, mut_coding, mut_protein, mut_effect, rep_patients
+#For BrS
+SELECT mut_chromosome, mut_chr_start_source, mut_id, mut_coding
 FROM ICC_MUTATIONS.report, ICC_MUTATIONS.gene, ICC_MUTATIONS.mutation
-WHERE rep_ref_id=665 AND gene_id=mut_gene_id AND mut_id=rep_mut_id AND mut_effect = "missense";
-SELECT gene_symbol, mut_coding, mut_protein, mut_effect, mut_exac_frequency, rep_patients
-from report, gene, mutation
-where rep_ref_id=665 and gene_id=mut_gene_id and mut_id=rep_mut_id;
+WHERE rep_ref_id = 713 AND gene_id=mut_gene_id AND mut_id=rep_mut_id AND mut_effect = "missense";# AND mut_coding NOT LIKE "%del%";
 
 
-SELECT * FROM ICC_MUTATIONS.report, ICC_MUTATIONS.gene, ICC_MUTATIONS.mutation;
+
+#SELECT gene_symbol, mut_id, mut_coding, mut_protein, mut_effect, rep_patients
+#FROM ICC_MUTATIONS.report, ICC_MUTATIONS.gene, ICC_MUTATIONS.mutation
+#WHERE rep_ref_id=665 AND gene_id=mut_gene_id AND mut_id=rep_mut_id AND mut_effect = "missense";
+#SELECT gene_symbol, mut_coding, mut_protein, mut_effect, mut_exac_frequency, rep_patients
+#from report, gene, mutation
+#where rep_ref_id=665 and gene_id=mut_gene_id and mut_id=rep_mut_id;
 
 
-SELECT * FROM ICC_MUTATIONS.report, ICC_MUTATIONS.gene;
-SELECT DISTINCT rep_disease FROM ICC_MUTATIONS.report;
+#SELECT * FROM ICC_MUTATIONS.report, ICC_MUTATIONS.gene, ICC_MUTATIONS.mutation;
+
+
+#SELECT * FROM ICC_MUTATIONS.report, ICC_MUTATIONS.gene;
+#SELECT DISTINCT rep_disease FROM ICC_MUTATIONS.report;
