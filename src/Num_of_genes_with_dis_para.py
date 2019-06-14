621768cb57fd80ac305759c9e82ed682bd65e3d6
nli8888 <nli8888@hotmail.com>
2019-06-14 16:44:30 +0100

    updated

--- src/Num_of_genes_with_dis_para.py
+++ src/Num_of_genes_with_dis_para.py
@@ -17,64 +17,29 @@ with open(input_file, "r", encoding="utf-8") as f:
 			paralog_codons = line.split("PARALOGS->")[1]
 			gene = paralog_codons.split("|")[3]
 			
-			out_file.write(str(chrom)+" "+str(position)+"\t"+str(ID)+"\t"+str(gene)+"\t"+str(Ref)+"\t"+str(Alt)+"\t")
+			# out_file.write(str(chrom)+" "+str(position)+"\t"+str(ID)+"\t"+str(gene)+"\t"+str(Ref)+"\t"+str(Alt)+"\t")
 
 			paralog_codons = paralog_codons.split("|&")[1]
 			paralog_codons = paralog_codons.split("&")
 			paralog_codons.remove("")
-			refid_flav2_check = 0
-			if refid_flavour == "all_con":
-				if all(refids.endswith("REFID=1") for refids in paralog_codons) == True:
-					refid_flav2_check = 1
+			
 
 			for codon in paralog_codons:
 
-				if refid_flavour == "noQC" or refid_flav2_check == 1:
-					refID = codon.split(":")[-1]
-					codon = codon.split(":")[1]
-					paralog_chrom = codon.split("_")[0].lstrip("chr")
-					paralog_position = int(codon.split("-")[1])
+				refID = codon.split(":")[-1]
+				paralog_gene = codon.split(":")[0]
+				codon = codon.split(":")[1]
+				paralog_chrom = codon.split("_")[0].lstrip("chr")
+				paralog_position = int(codon.split("-")[1])
 
-					out_file.write(
-						str(paralog_chrom)+" "+str(paralog_position-2)+"\t"+
-						str(paralog_chrom)+" "+str(paralog_position-1)+"\t"+
-						str(paralog_chrom)+" "+str(paralog_position)+"\t"
-						)
+			# 	out_file.write(
+			# 		str(paralog_chrom)+" "+str(paralog_position-2)+"\t"+
+			# 		str(paralog_chrom)+" "+str(paralog_position-1)+"\t"+
+			# 		str(paralog_chrom)+" "+str(paralog_position)+"\t"
+			# 		)
 
-				if refid_flavour == "para_con":
-					refID = codon.split(":")[-1]
-					if refID == "REFID=1":
-						codon = codon.split(":")[1]
-						paralog_chrom = codon.split("_")[0].lstrip("chr")
-						paralog_position = int(codon.split("-")[1])
+			# out_file.write("\n")
 
-						out_file.write(
-							str(paralog_chrom)+" "+str(paralog_position-2)+"\t"+
-							str(paralog_chrom)+" "+str(paralog_position-1)+"\t"+
-							str(paralog_chrom)+" "+str(paralog_position)+"\t"
-							)
-
-			out_file.write("\n")
-
-			if len(paralog_codons)*3 + 3 > max_split:
-				max_split = len(paralog_codons)*3 + 3
-				max_paralog_codons = paralog_codons
-	# print("THE MAX NUMBER OF COLUMNS: ",max_split, max_paralog_codons)	#number of columns for dataframe in R; + 2 for (chrom poistion) and (ID)
-out_file.close()
-
-#Remove last "\n" from file so R doesn't think its a separate column when reading in
-# infile = input_file+"2.noQC" #paralogs2 file
-
-out_file_name = infile+"woLastCol"
-out_file = open(infile+"woLastCol", "w")
-with open(infile) as f:
-	for line in f:
-		line = line.rstrip()
-		out_file.write(str(line)+"\n")
-
-out_file.close()
-os.system("mv " + out_file_name + " " + infile)
-
-print("File_prep_for_R Done!")
+			
 
 
