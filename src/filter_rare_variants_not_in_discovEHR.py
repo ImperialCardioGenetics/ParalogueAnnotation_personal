d5a3ed5dff91626e5770d880425b3127102af8cd
Nicholas Li <nyl112@login-6.cx1.hpc.ic.ac.uk>
2019-08-14 00:08:07 +0100

    updated

--- src/filter_rare_variants_not_in_discovEHR.py
+++ src/filter_rare_variants_not_in_discovEHR.py
@@ -14,35 +14,50 @@ tabix_file = pysam.TabixFile(input_file1)
 
 discovEHR_file = pysam.TabixFile("/work/nyl112/data/DiscovEHR/discovEHR_GRCh37.vcf.gz")
 
-def get_info_value(db_file,chrom,pos,ref_nt,alt_nt,ref_aa,alt_aa):
-    # position_found = False
-    REVEL= ""
-    for row in db_file.fetch(chrom, pos - 1, pos):
-        row_column = row.split('\t')
-        if str(pos) != row_column[1]:
-            continue
-        # position_found = True
-        row_ref_nt = row_column[2]
-        row_alt_nt = row_column[3]
-        row_ref_aa = row_column[4]
-        row_alt_aa = row_column[5]
-        #print row_ref_nt,row_alt_nt,row_ref_aa,row_alt_aa
-        #print ref_nt,alt_nt,ref_aa,alt_aa
-        if row_ref_nt == ref_nt and row_alt_nt == alt_nt and row_ref_aa == ref_aa and row_alt_aa ==alt_aa:
-            REVEL = row_column[6]
-        elif row_ref_nt == ref_nt and row_alt_nt == alt_nt:
-            REVEL = row_column[6]
-    # print(REVEL)
-    while not isinstance(REVEL, str):
-        REVEL = REVEL[0]
-    REVEL = [REVEL]
-    return REVEL
+# def get_info_value(db_file,chrom,pos,ref_nt,alt_nt,ref_aa,alt_aa):
+#     # position_found = False
+#     REVEL= ""
+#     for row in db_file.fetch(chrom, pos - 1, pos):
+#         row_column = row.split('\t')
+#         if str(pos) != row_column[1]:
+#             continue
+#         # position_found = True
+#         row_ref_nt = row_column[2]
+#         row_alt_nt = row_column[3]
+#         row_ref_aa = row_column[4]
+#         row_alt_aa = row_column[5]
+#         #print row_ref_nt,row_alt_nt,row_ref_aa,row_alt_aa
+#         #print ref_nt,alt_nt,ref_aa,alt_aa
+#         if row_ref_nt == ref_nt and row_alt_nt == alt_nt and row_ref_aa == ref_aa and row_alt_aa ==alt_aa:
+#             REVEL = row_column[6]
+#         elif row_ref_nt == ref_nt and row_alt_nt == alt_nt:
+#             REVEL = row_column[6]
+#     # print(REVEL)
+#     while not isinstance(REVEL, str):
+#         REVEL = REVEL[0]
+#     REVEL = [REVEL]
+#     return REVEL
 
 chrom = 1
-pos = 69088
+pos = 69082
 
-for row in tabix_file.fetch(chrom, pos - 1, pos):
-	print(row.split()[1])
+
+for pos in range(69080,69083):
+	check = 0
+	for row in tabix_file.fetch(chrom, pos - 1, pos):
+		print(row)
+		row_pos = int(row.split()[1])
+		print(pos, row_pos)
+		if pos == row_pos:
+			print("TRUE")
+			check = 1
+		elif pos != row_pos:
+			print("FALSE")
+
+	if check == 1:
+		print("CHECK PASSED")
+	else:
+		print("CHECK FAILED")
 
         # if line.startswith("CHROM"):
         # 	out_file.write(line.rstrip()+"\tREVEL_score\n")
