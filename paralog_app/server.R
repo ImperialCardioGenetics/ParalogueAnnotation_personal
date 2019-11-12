20e4e2485842b15bf9d059912c41f4f31f1ff9cb
Pantazis Theotokis <pan.theo@hotmail.com>
2019-11-12 18:14:42 +0000

    added ENSG link to alignments

--- paralog_app/server.R
+++ paralog_app/server.R
@@ -6,6 +6,10 @@ library(shinythemes)
 
 shinyServer(function(input, output){
   
+  #read gene symbol/ENSG and write to dict
+  mart_export <- read.delim("data/mart_export.txt", quote="", stringsAsFactors=FALSE)
+  map=setNames(mart_export$Gene.stable.ID, mart_export$HGNC.symbol)
+  
   get_paralog<-function(){
     
     #input<-data.frame(chr="1",pos="114713907",ref="T",alt="A")
@@ -27,13 +31,26 @@ shinyServer(function(input, output){
       input_data<-data.frame(mutation=var)
       colnames(input_data)<-"mutation"
       result<-predict_output(raw_data,input_data)
+    }else{
+        if (input$format == 'upload') {
+          req(input$file)
+          inFile <- input$file
+          input_file = read.table(inFile$datapath)
+          colnames(input_file) <- "mutation"
+          result <- predict_output(raw_data, input_file)
+        }
       }
     }
+  
+    #add ClinVar IDs with URLs 
+    result$Query_ClinVar<- paste0("<a href='", paste0("https://www.ncbi.nlm.nih.gov/clinvar/variation/",result$Query_ClinVar,"/"), "' target='_blank'>", result$Query_ClinVar, "</a>")  
+    result$ClinVar_ID<- paste0("<a href='", paste0("https://www.ncbi.nlm.nih.gov/clinvar/variation/",result$ClinVar_ID,"/"), "' target='_blank'>", result$ClinVar_ID, "</a>")  
     
-  result$Query_ClinVar<- paste0("<a href='", paste0("https://www.ncbi.nlm.nih.gov/clinvar/variation/",result$Query_ClinVar,"/"), "' target='_blank'>", result$Query_ClinVar, "</a>")  
-  result$ClinVar_ID<- paste0("<a href='", paste0("https://www.ncbi.nlm.nih.gov/clinvar/variation/",result$ClinVar_ID,"/"), "' target='_blank'>", result$ClinVar_ID, "</a>")  
-    
-  return(result)
+    #generate ensembl alignment URLs
+    # https://www.ensembl.org/Homo_sapiens/Gene/Compara_Paralog/Alignment?db=core;g=ENSG00000213281;g1=ENSG00000133703;seq=cDNA
+    result$Ensembl_alignment<- paste0("<a href='", paste0("https://www.ensembl.org/Homo_sapiens/Gene/Compara_Paralog/Alignment?db=core;g=",map[unlist(result$Query_Gene)],";g1=",map[unlist(result$Gene)],";seq=cDNA"), "' target='_blank'>alignment</a>")  
+  
+    return(result)
   }
   
   output$paralog<-renderDataTable(DT::datatable(get_paralog(),
