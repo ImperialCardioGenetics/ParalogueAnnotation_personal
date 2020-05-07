59fc271e88d8c10adedfb2aa1d37a49493043d89
nli8888 <nli8888@hotmail.com>
2020-05-07 12:02:43 +0100

    created Tableize_wrapper_for_Rshiny_app.py

---
+++ src/Tableize_wrapper_for_Rshiny_app.py
@@ -0,0 +1,64 @@
+import os, sys, subprocess, re
+
+#Wrapper for tableize that adds in features used for R shiny app. Join the results generated from here to old paralogues results generated normally.
+#Tableize.py wrapper plus additional formatting that can't be done in tableize. Also adds in Para Z scores.
+
+def Tableize_wrap(input_file, tableize_dir, para_zscore_dir):
+	#input_file = sys.argv[1]	#path of paraloc file e.g. /data/Share/nick/Paralog_Anno/data_files/clinvar_20171029_onlyPathogenic.out_paraloc
+	dir1 = input_file.rsplit("/", 1)[0]
+
+	if tableize_dir.endswith("/"):
+		tableize_dir = tableize_dir
+	else:
+		tableize_dir = tableize_dir + "/"
+
+	if para_zscore_dir.endswith("/"):
+		para_zscore_dir = para_zscore_dir
+	else:
+		para_zscore_dir = para_zscore_dir + "/"
+
+	os.system("python2 " + tableize_dir + "tableize_vcf.py --vcf " + input_file + " --out " + input_file + "_tableized_org --do_not_minrep --include_id --vep_info SYMBOL,Protein_position,Amino_acids,Codons,BIOTYPE,Paralogue_Vars --split_by_transcript --canonical_only")
+
+	out_file = open(input_file + "_tableized", "w")
+	with open(input_file + "_tableized_org") as f:
+		for line in f:
+			if line.startswith("CHROM"):
+				out_file.write(line.rstrip()+"\tPara_Z_score\n")
+			else:
+				# print(line)
+				line = line.split()
+				AAs = line[8].split(",")
+				Codons = line[9].split(",")
+				Gene = str(line[6])+".txt"
+				BIOTYPE = line[10]
+				if not BIOTYPE == "protein_coding":
+					continue
+				for column in range(0,len(AAs)):
+					if (line[7] != "NA" and 
+						not "-" in line[7]):
+						# print("A", Gene)
+						if Gene in os.listdir(para_zscore_dir + "para_zscores/genes"):
+							# print("B")
+							with open(para_zscore_dir + "para_zscores/genes/" + Gene) as para_z_file:
+								# print("B.1")
+								for i, para_z_line in enumerate(para_z_file):
+									# print(i, int(line[7])-1)
+									if i < int(line[7])-1:
+										para_z_score = "NA"
+									elif i == int(line[7])-1:
+										# print("C")
+										if AAs[column].split("/")[0] == para_z_line.split()[1]:
+											# print("D")
+											para_z_score = para_z_line.split()[2]
+										else:
+											para_z_score = "NA"
+									elif i > int(line[7])-1:
+										break
+						else:
+							para_z_score = "NA"
+					else:
+						para_z_score = "NA"
+					out_file.write(line[0] + "\t" + line[1] +"\t" +line[2] +"\t" + line[3] +"\t" +line[4] +"\t" +line[5] +"\t" + line[6] + "\t" + line[7] + "\t" + AAs[column] + "\t" + Codons[column] + "\t" + line[10] + "\t" + line[11] + "\t" + str(para_z_score) + "\n")
+		out_file.close()
+
+	print("Tableize_wrapper_for_Rshiny_app Done!")
