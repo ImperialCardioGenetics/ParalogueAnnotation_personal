9d7f0f088f1508362fc1a7ca54c523e6b243ac21
nli8888 <nli8888@hotmail.com>
2022-06-09 18:40:20 +0100

    updated

--- src/Paralogous_var_align_functions.R
+++ src/Paralogous_var_align_functions.R
@@ -419,6 +419,55 @@ conf_matrix = function(ptop.num_of_paralog_anno, p.paralog_data, btop.num_of_par
               ))
 }
 
+conf_matrix_USED_FOR_CORRECTIONS = function(ptop.num_of_paralog_anno, p.paralog_data, btop.num_of_paralog_anno, b.paralog_data){ #function for calculating confusion matrix and stats
+  con_table_TP = ptop.num_of_paralog_anno
+  con_table_FP = btop.num_of_paralog_anno
+  con_table_TN = b.paralog_data-btop.num_of_paralog_anno
+  con_table_FN = p.paralog_data-ptop.num_of_paralog_anno
+  con_table_PPV = con_table_TP/(con_table_TP+con_table_FP)
+  
+  PPV_SE = sqrt((con_table_PPV*(1-con_table_PPV))/(con_table_TP+con_table_FP))
+  PPV_CI = c(con_table_PPV - (1.96*PPV_SE), con_table_PPV + (1.96*PPV_SE))
+  
+  con_table_Sensitivty = con_table_TP/(con_table_TP+con_table_FN)
+  
+  Sens_SE = sqrt((con_table_Sensitivty*(1-con_table_Sensitivty))/(con_table_TP+con_table_FN))
+  Sens_CI = c(con_table_Sensitivty - (1.96*Sens_SE), con_table_Sensitivty + (1.96*Sens_SE))
+  
+  con_table_Specificity = con_table_TN/(con_table_TN+con_table_FP)
+  con_table_FPR = con_table_FP/(con_table_FP+con_table_TN)
+  con_table_ACC = (con_table_TP + con_table_TN)/(con_table_TP + con_table_TN + con_table_FP + con_table_FN)
+  con_table = matrix(
+    c(p.paralog_data-ptop.num_of_paralog_anno,
+      ptop.num_of_paralog_anno,
+      b.paralog_data-btop.num_of_paralog_anno,
+      btop.num_of_paralog_anno
+    ), ncol = 2
+  )
+  colnames(con_table) = c("Pathogenic", "Benign")
+  rownames(con_table) = c("Number of variants not predicted as pathogenic", "Number of variants predicted as pathogenic")
+  con_table_p_value = fisher.test(con_table)
+  # con_table_MCC = ((con_table_TP*con_table_TN)-(con_table_FP*con_table_FN))/sqrt((con_table_TP+con_table_FP)*(con_table_TP+con_table_FN)*(con_table_TN+con_table_FP)*(con_table_TN+con_table_FN))
+  return(list("con_table" = con_table, 
+              "Accuracy" = con_table_ACC, 
+              "PPV" = con_table_PPV,
+              "PPV_SE" = PPV_SE,
+              "PPV_CI" = PPV_CI,
+              "Sensitivity" = con_table_Sensitivty, 
+              "Sensitivity_SE" = Sens_SE,
+              "Sensitivity_CI" = Sens_CI,
+              "Specificity" = con_table_Specificity, 
+              "FPR" = con_table_FPR, 
+              "Pvalue" = con_table_p_value, 
+              "TP" = con_table_TP, 
+              "FP" = con_table_FP, 
+              "TN" = con_table_TN, 
+              "FN" = con_table_FN
+              # "MCC" = con_table_MCC
+  ))
+}
+
+
 conf_matrix_benign = function(ptob.num_of_paralog_anno, p.paralog_data, btob.num_of_paralog_anno, b.paralog_data){ #function for calculating confusion matrix and stats
   con_table_TP = btob.num_of_paralog_anno
   con_table_FP = ptob.num_of_paralog_anno
