6c734326de80e731ad65e19131a098e066154be2
nli8888 <nli8888@hotmail.com>
2023-11-24 06:00:02 +0800

    updated Annotate_dbNSFPv4.5.sh

--- src/Annotate_dbNSFPv4.5.sh
+++ src/Annotate_dbNSFPv4.5.sh
@@ -1,7 +1,7 @@
 #PBS -lwalltime=72:0:0
-#PBS -lselect=1:ncpus=1:mem=4gb
+#PBS -lselect=1:ncpus=1:mem=10gb
 
 module load anaconda3/personal
 source /home/nyl112/.bashrc
 
-python /work/nyl112/Pfams/Pfam_meta_domains/src/Create_pfam_domain_mapping_objects.py /rds/general/project/lms-ware-analysis/live/nick/metadomains
+java -Xmx5g search_dbNSFP45a -i /rds/general/project/lms-ware-analysis/live/nick/ParalogueAnnotation_personal/data/clinvar/clinvar_20190114_GRCh38_onlyPathogenic_and_Likely_pathogenic.vcf -o /rds/general/project/lms-ware-analysis/live/nick/dbNSFP4.5/clinvar_20190114_GRCh38_onlyPathogenic_and_Likely_pathogenic.vcf.out -p -w 1-6,59-61,77-79,96-101,108-110,122-140
