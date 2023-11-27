d8fb3969b74ec8db75fda6bd14d282265928670d
nli8888 <nli8888@hotmail.com>
2023-11-27 23:13:46 +0800

    updated

--- src/Annotate_clinvar_patho_dbNSFPv4.5.sh
+++ src/Annotate_clinvar_patho_dbNSFPv4.5.sh
@@ -5,4 +5,4 @@ module load anaconda3/personal
 source /home/nyl112/.bashrc
 
 cd /rds/general/project/lms-ware-analysis/live/nick/dbNSFP4.5 
-java -Xmx5g search_dbNSFP45a -i /rds/general/project/lms-ware-analysis/live/nick/ParalogueAnnotation_personal/data/clinvar/clinvar_20190114_GRCh38_onlyPathogenic_and_Likely_pathogenic.vcf -o /rds/general/project/lms-ware-analysis/live/nick/dbNSFP4.5/clinvar_20190114_GRCh38_onlyPathogenic_and_Likely_pathogenic.vcf.out -p -w 1-6,38-40,59-61,77-84,96-101,108-110,122-140,147-149
+java -Xmx5g search_dbNSFP45a -i /rds/general/project/lms-ware-analysis/live/nick/ParalogueAnnotation_personal/data/clinvar/clinvar_20190114_GRCh38_onlyPathogenic_and_Likely_pathogenic.vcf -o /rds/general/project/lms-ware-analysis/live/nick/dbNSFP4.5/dbNSFP45a_clinvar_20190114_GRCh38_onlyPathogenic_and_Likely_pathogenic.vcf.out -p -w 1-6,38-40,59-61,77-84,96-101,108-110,122-140,147-149
