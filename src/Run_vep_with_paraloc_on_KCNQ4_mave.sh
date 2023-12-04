390ca1304420f13e83c40315da383eaeff951443
nli8888 <nli8888@hotmail.com>
2023-12-04 17:54:32 +0800

    updated

---
+++ src/Run_vep_with_paraloc_on_KCNQ4_mave.sh
@@ -0,0 +1,11 @@
+#PBS -lwalltime=72:0:0
+#PBS -lselect=1:ncpus=1:mem=16gb
+
+module load anaconda3/personal
+source /home/nyl112/.bashrc
+
+/home/nyl112/perl5/perlbrew/bin/perlbrew switch perl-5.22.0
+
+perl -I /work/nyl112/paralogueAnnotator/ /work/nyl112/ensembl-vep/vep --force_overwrite --vcf --allele_number --canonical --offline --cache --dir_cache /work/nyl112/ -assembly GRCh38 -i /rds/general/project/lms-ware-analysis/live/nick/ParalogueAnnotation_personal/data/otherMaveDB_score/KCNQ4_mave_benign_b38.vcf -o /rds/general/project/lms-ware-analysis/live/nick/ParalogueAnnotation_personal/data/otherMaveDB_score/KCNQ4_mave_benign_b38.out_paraloc --plugin ParalogueAnnotation,paraloc
+
+perl -I /work/nyl112/paralogueAnnotator/ /work/nyl112/ensembl-vep/vep --force_overwrite --vcf --allele_number --canonical --offline --cache --dir_cache /work/nyl112/ -assembly GRCh38 -i /rds/general/project/lms-ware-analysis/live/nick/ParalogueAnnotation_personal/data/otherMaveDB_score/KCNQ4_mave_deleterious_b38.vcf -o /rds/general/project/lms-ware-analysis/live/nick/ParalogueAnnotation_personal/data/otherMaveDB_score/KCNQ4_mave_deleterious_b38.out_paraloc --plugin ParalogueAnnotation,paraloc
