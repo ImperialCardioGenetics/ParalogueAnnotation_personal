b8b61f2c24414791b05941c3f3fd6b69fe444da8
nli8888 <nli8888@hotmail.com>
2023-12-07 03:26:32 +0800

    updated

--- src/Annotate_TP53_dbNSFPv4.5.sh
+++ src/Annotate_TP53_dbNSFPv4.5.sh
@@ -5,6 +5,6 @@ module load anaconda3/personal
 source /home/nyl112/.bashrc
 
 cd /rds/general/project/lms-ware-analysis/live/nick/dbNSFP4.5 
-java -Xmx5g search_dbNSFP45a -i /rds/general/project/lms-ware-analysis/live/nick/ParalogueAnnotation_personal/data/TP53-TA/4704961/TP53-TA_deleterious.vcf -o /rds/general/project/lms-ware-analysis/live/nick/dbNSFP4.5/dbNSFP45a_TP53-TA_deleterious.vcf.out -p -w 1-6,28,38-40,59-61,77-84,96-101,108-110,122-140,147-149
+java -Xmx5g search_dbNSFP45a -v hg19 -i /rds/general/project/lms-ware-analysis/live/nick/ParalogueAnnotation_personal/data/TP53-TA/4704961/TP53-TA_deleterious.vcf -o /rds/general/project/lms-ware-analysis/live/nick/dbNSFP4.5/dbNSFP45a_TP53-TA_deleterious.vcf.out -p -w 1-6,28,38-40,59-61,77-84,96-101,108-110,122-140,147-149
 
-java -Xmx5g search_dbNSFP45a -i /rds/general/project/lms-ware-analysis/live/nick/ParalogueAnnotation_personal/data/TP53-TA/4704961/TP53-TA_benign.vcf -o /rds/general/project/lms-ware-analysis/live/nick/dbNSFP4.5/dbNSFP45a_TP53-TA_benign.vcf.out -p -w 1-6,28,38-40,59-61,77-84,96-101,108-110,122-140,147-149
+java -Xmx5g search_dbNSFP45a -v hg19 -i /rds/general/project/lms-ware-analysis/live/nick/ParalogueAnnotation_personal/data/TP53-TA/4704961/TP53-TA_benign.vcf -o /rds/general/project/lms-ware-analysis/live/nick/dbNSFP4.5/dbNSFP45a_TP53-TA_benign.vcf.out -p -w 1-6,28,38-40,59-61,77-84,96-101,108-110,122-140,147-149
