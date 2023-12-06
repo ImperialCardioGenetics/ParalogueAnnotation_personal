c13de41019ea23f63a0bf9fadb7efbc524789121
nli8888 <nli8888@hotmail.com>
2023-12-06 16:56:52 +0800

    updated

--- src/Annotate_KCNQ4_dbNSFPv4.5.sh
+++ src/Annotate_KCNQ4_dbNSFPv4.5.sh
@@ -5,6 +5,6 @@ module load anaconda3/personal
 source /home/nyl112/.bashrc
 
 cd /rds/general/project/lms-ware-analysis/live/nick/dbNSFP4.5 
-java -Xmx5g search_dbNSFP45a -i /rds/general/project/lms-ware-analysis/live/nick/ParalogueAnnotation_personal/data/otherMaveDB_score/KCNQ4_mave_deleterious_b38.vcf -o /rds/general/project/lms-ware-analysis/live/nick/dbNSFP4.5/dbNSFP45a_KCNQ4_mave_deleterious_b38.vcf.out -p -w 1-6,38-40,59-61,77-84,96-101,108-110,122-140,147-149
+java -Xmx5g search_dbNSFP45a -i /rds/general/project/lms-ware-analysis/live/nick/ParalogueAnnotation_personal/data/otherMaveDB_score/KCNQ4_mave_deleterious_b38.vcf -o /rds/general/project/lms-ware-analysis/live/nick/dbNSFP4.5/dbNSFP45a_KCNQ4_mave_deleterious_b38.vcf.out -p -w 1-6,28,38-40,59-61,77-84,96-101,108-110,122-140,147-149
 
-java -Xmx5g search_dbNSFP45a -i /rds/general/project/lms-ware-analysis/live/nick/ParalogueAnnotation_personal/data/otherMaveDB_score/KCNQ4_mave_benign_b38.vcf -o /rds/general/project/lms-ware-analysis/live/nick/dbNSFP4.5/dbNSFP45a_KCNQ4_mave_benign_b38.vcf.out -p -w 1-6,38-40,59-61,77-84,96-101,108-110,122-140,147-149
+java -Xmx5g search_dbNSFP45a -i /rds/general/project/lms-ware-analysis/live/nick/ParalogueAnnotation_personal/data/otherMaveDB_score/KCNQ4_mave_benign_b38.vcf -o /rds/general/project/lms-ware-analysis/live/nick/dbNSFP4.5/dbNSFP45a_KCNQ4_mave_benign_b38.vcf.out -p -w 1-6,28,38-40,59-61,77-84,96-101,108-110,122-140,147-149
